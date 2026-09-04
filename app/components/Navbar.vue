e531474842ba917cb6e227ec224136fc9da00c92
TorMagne <tormagne-brekken@outlook.com>
2026-09-04 13:23:06 +0200

    setting up the project and testing small things

---
+++ app/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <div>
+      <h1>REALMS of ELYNDOR</h1>
+    </div>
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <Navbar />
+  <button
+    class="border border-game-border-gold bg-game-gold-dark px-5 py-2 font-fantasy text-game-white transition hover:bg-game-gold"
+  >
+    Investigate
+  </button>
+</template>
